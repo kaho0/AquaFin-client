d07fcf6f6d1d7a998f31fa7b3d99569e34d0b8b9
kahon <kahonbintezaman@gmail.com>
2025-03-04 01:25:24 +0600

    deploy hoi ja bhai please

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["firebase/app"], // Ensure Firebase is not bundled incorrectly
+    },
+  },
 });
