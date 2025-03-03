d5c9d2adc46f9939481834a2294cc4c599261e63
kahon <kahonbintezaman@gmail.com>
2025-03-04 01:49:30 +0600

    jsx updated

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -3,7 +3,7 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./components/firebase.js";
 import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
-import SignInwithGoogle from "./components/signInWithGoogle";
+import SignInwithGoogle from "./components/SignInwithGoogle.jsx";
 
 const Login = () => {
   const [email, setEmail] = useState("");
